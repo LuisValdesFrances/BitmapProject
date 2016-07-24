c370c7eb21513b58dec29f7ac867f7b7e9f0460e
Luis <triskel67@hotmail.com>
2016-07-24 18:54:39 +0200

    First commit

---
+++ bitmap.h
@@ -0,0 +1,68 @@
+#ifndef __BITMAP_H__
+#define __BITMAP_H__
+
+#define FALSE 0
+#define TRUE  1
+
+#define BMP_HEADER_SIZE 14
+#define DIB_HEADER_SIZE 40
+#define OFFSET_PIXEL_ARRAY (SIZE_BMP_HEADER + SIZE_DIB_HEADER)
+#define NUMBER_BITS_PIXEL 24
+
+#define PIXEL_IMAGE_SIZE 3
+#define PADDING_IMAGE_SIZE 2
+
+/*
+Duda, cada fila del archivo .bmp tiene un tamaño determinado. Esta informacion es inherente al
+propio formato .bmp ¿Necesito almanezar esta informacion en algún lado definido en esta interface?
+
+const unsigned int BIMAP_ROW_SIZE[] = {
+	//BMP HEADER
+	2, //"BM"
+	4, //Size of BMP file
+	2, //Application specefic
+	2, //Application specefic
+	4, //Offset pixel array start
+	//DIB HEADER
+	4, //Number of bits in DIB header
+	...
+
+
+}
+
+*/
+
+/*Abstract data structure*/ 
+typedef struct _bitmap *Bitmap;
+/*
+	int[] BMP_Header
+	int[] DIB_Header
+	int[] pixelImage
+
+*/
+Bitmap buildBitmap(int imageWidth, int imageHeight);
+/*
+	BMP_Header[SIZE_BMP_HEADER]
+	BMP_Header[SIZE_DIB_HEADER]
+	BMP_Header[getSizeImage(imageWidth, imageHeight)]
+*/
+
+/**
+Get image size (including padding)
+*/
+int getImageSize(int imageWidth, int imageHeight)
+
+/**
+Get char array of hexadecimnal value. The array size is determite for rowSize
+*/
+*char getHexValue(int value, int rowSize)
+
+/**
+Devuelve un array de valores hexadecimales, con la informacion de color para un pixel
+El parametro de entrada es un array de enteros, con la informacion del color de un pixel en formato decimal
+[255,0,0] -> [FF,0,0]
+*/
+
+*char getGreenComponent(*int color);
+*char getRedComponent(*int color);
+*char getBlueComponent(*int color);
