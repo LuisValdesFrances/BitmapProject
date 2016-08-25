cef1adc6bd66b1dd9794703bbc46e4ed2ec8e821
Luis <triskel67@hotmail.com>
2016-08-25 13:42:42 +0200

    Create hear and dib implementation. Print function implementation

--- bitmap.c
+++ bitmap.c
@@ -11,7 +11,7 @@ struct _bitmap{
 };
 
 
-struct _header {
+struct _header{
 	unsigned char signature[2];
 	unsigned char size[4];
 	unsigned char unused1[2];
@@ -19,23 +19,68 @@ struct _header {
 	unsigned char pixelArrayOffset[4];
 };
 
+struct _dib{
+	unsigned char dibSize[4];
+	unsigned char bitmapWidth[4];
+	unsigned char bitmapHeight[4];
+	unsigned char numberColors[2];
+	unsigned char NumberBitsPerPixel[2];
+	unsigned char biRGB[4];
+	unsigned char bitmapSize[4];
+	unsigned char printResolutionHorizontal[4];
+	unsigned char printResolutionVertical[4];
+	unsigned char colorsPalette[4];
+	unsigned char importantColors[4];
+};
+
 void printHeader(struct _header header);
-void printResult(struct _header header, struct _bitmap bitmap);
+void printDib(struct _dib dib);
+void printResult(struct _header header, struct _dib dib, Bitmap bitmap);
 int getPading(int width);
-void printBitmap(struct _bitmap bitmap);
+void setBitmapSize(struct _dib *dib, Bitmap bitmap);
+void setBitmapDimensions(struct _dib *dib, Bitmap bitmap);
+void printHeader(struct _header header);
+void printDib(struct _dib dib);
+void printBitmap(Bitmap bitmap);
 
 
 Bitmap buildBitmap(int width, int height){
+	/*
+	La idea es alojar una serie de datos, los que componen un bitmap en la memoria heap. Así que el 
+	objetivo de este metodo es crear espacio en la memoria heap, el suficiente para que pueda almacenar
+	un bitmap del tamapño especificado por los parametros.
+
+	Asi pues, buildBitmap hace lo siguiente:
+		-Reserva espacio en la memoria heap para almacenar un Bitmap
+		-Reserva otro espacio en la memoria heap`para almacenar una lista de colores
+		-Accede al la lista de colores del bitmap (puntero) y la cambia el valor para que apunte al espacio donde se
+		 almacenan los colores.
+		-Devuelve un puntero que apunte a dicho espacia en la memoria heap
+	*/
+
+	/*
+	Partiendo de esto, creo un puntero que apunta a ese lugar de la heap. Es importante matizar que el puntero
+	se aloja en la memoria frame, puesto que lo unico que estoy haciendo es reservar espacio en la memoria heap
+	*/
 	Bitmap bitmap = malloc(sizeof(struct _bitmap));
 	assert(bitmap != NULL);
 
+	//Almaceno en el espacio para el bitmap, en la heap, el ancho y alto de la imagen.
 	bitmap->width = width;
 	bitmap->height = height;
 	int size = width*height;
-	color *pixels = malloc(sizeof(struct _color)*size);
-	assert(pixels != NULL);
-
-	bitmap->pixels = pixels;
+	
+	/*
+	En otro lugar de la memoria heap creo el tamaño suficiente como para alojar una lista de pixeles
+	*/
+	color *p = malloc(sizeof(struct _color)*size);
+	assert(p != NULL);
+	/*
+	Vinculo ambas direcciones, es decir, accedo al valor del la lista de pixeles del bitmap y le cambio el
+	valor para que apunte al espacio de la heap, que acabo de crear, que es el que usare para almacenar 
+	la lista de pixeles
+	*/
+	bitmap->pixels = p;
 
 	return bitmap;
 }
@@ -47,6 +92,7 @@ void setPixel(Bitmap bitmap, int x, int y, color c){
 	printf("\nInsertado color en la posicion: %d", index);
 }
 
+//Header fuctions setters
 static void setSignature(struct _header *header){
 	header->signature[0] = 'B';
 	header->signature[1] = 'M';
@@ -70,6 +116,60 @@ void setUnused(struct _header *header){
 	header->unused2[1] = 0;
 }
 
+//DIB Header fuctions setters
+void setDibSize(struct _dib *dib, int size){
+	dib->dibSize[0] = size & 0xff;
+	dib->dibSize[1] = (size>>8) & 0xff;
+	dib->dibSize[2] = (size>>16) & 0xff;
+	dib->dibSize[3] = (size>>24) & 0xff;
+}
+void setBitmapDimensions(struct _dib *dib, Bitmap bitmap){
+	dib->bitmapWidth[0] = bitmap->width & 0xff;
+	dib->bitmapWidth[1] = (bitmap->width>>8) & 0xff;
+	dib->bitmapWidth[2] = (bitmap->width>>16) & 0xff;
+	dib->bitmapWidth[3] = (bitmap->width>>24) & 0xff;
+	dib->bitmapHeight[0] = bitmap->height & 0xff;;
+	dib->bitmapHeight[1] = (bitmap->height>>8) & 0xff;
+	dib->bitmapHeight[2] = (bitmap->height>>16) & 0xff;
+	dib->bitmapHeight[3] = (bitmap->height>>24) & 0xff;
+}
+void setBitmapSize(struct _dib *dib, Bitmap bitmap){
+	int imageSize = (bitmap->width*bitmap->height) * 3;
+	int paddingSize = (getPading(bitmap->width) * bitmap->height);
+	int totalSize = imageSize + paddingSize;
+	dib->bitmapSize[0] = totalSize & 0xff;
+	dib->bitmapSize[1] = (totalSize>>8) & 0xff;;
+	dib->bitmapSize[2] = (totalSize>>16) & 0xff;;
+	dib->bitmapSize[3] = (totalSize>>24) & 0xff;;
+}
+void setDibConstants(struct _dib *dib){
+	dib->numberColors[0] = 0;
+	dib->numberColors[1] = 1;
+	dib->NumberBitsPerPixel[0] = 24;
+	dib->NumberBitsPerPixel[1] = 0;
+	dib->biRGB[0] = 0;
+	dib->biRGB[1] = 0;
+	dib->biRGB[2] = 0;
+	dib->biRGB[3] = 0;
+	
+	dib->printResolutionHorizontal[0] = 2835 & 0xff;
+	dib->printResolutionHorizontal[1] = (2835>>8) & 0xff;
+	dib->printResolutionHorizontal[2] = (2835>>16) & 0xff;
+	dib->printResolutionHorizontal[3] = (2835>>24) & 0xff;
+	dib->printResolutionVertical[0] = 2835 & 0xff;
+	dib->printResolutionVertical[1] = (2835>>8) & 0xff;
+	dib->printResolutionVertical[2] = (2835>>16) & 0xff;
+	dib->printResolutionVertical[3] = (2835>>24) & 0xff;
+	dib->colorsPalette[0] = 0;
+	dib->colorsPalette[1] = 0;
+	dib->colorsPalette[2] = 0;
+	dib->colorsPalette[3] = 0;
+	dib->importantColors[0] = 0;
+	dib->importantColors[1] = 0;
+	dib->importantColors[2] = 0;
+	dib->importantColors[3] = 0;
+}
+
 void setPixelArrayOffset(struct _header *header, int pixelArrayOffset){
 	header->pixelArrayOffset[0] = pixelArrayOffset & 0xff;
 	header->pixelArrayOffset[1] = (pixelArrayOffset>>8) & 0xff;
@@ -86,7 +186,18 @@ static struct _header createHeader(int size, int pixelArrayOffset) {
 	setPixelArrayOffset(&header, pixelArrayOffset);
 
 	return header;
-}
+};
+
+static struct _dib createDib(int size, Bitmap bitmap) {
+
+	struct _dib dib;
+	setDibSize(&dib, size);
+	setBitmapDimensions(&dib, bitmap);
+	setBitmapSize(&dib, bitmap);
+	setDibConstants(&dib);
+
+	return dib;
+};
 
 
 void saveBitmap(Bitmap bitmap, char *filename){
@@ -102,26 +213,27 @@ void saveBitmap(Bitmap bitmap, char *filename){
 	int pixelArrayOffset = headerSize + dibSize;
 
 	struct _header header = createHeader(fileSize, pixelArrayOffset);
+	struct _dib dib = createDib(dibSize, bitmap);
 	/*
-	struct _dib dib = createDib(bitmap);
 	char *pixelArray = createPixelArray(bitmap);
 	//*/
 
 
-	printResult(header, *bitmap);
+	printResult(header, dib, bitmap);
 
 	//write();
 
 	//*/
 }
 
-void printResult(struct _header header, struct _bitmap bitmap){
+void printResult(struct _header header, struct _dib dib, Bitmap bitmap){
 	printHeader(header);
-	//printDib();
+	printDib(dib);
 	printBitmap(bitmap);
 }
 
 void printHeader(struct _header header){
+	printf("\n\n***HEADER***");
 	printf("\n-Header signature-");
 	printf("\n%c", header.signature[0]);
 	printf(",%c", header.signature[1]);
@@ -137,38 +249,101 @@ void printHeader(struct _header header){
 	printf(",%d", (int)header.unused2[1]);
 	printf("\n-Pixel array offset-");
 	printf("\n%d", (int)header.pixelArrayOffset[0]);
-	printf(",\n%d", (int)header.pixelArrayOffset[1]);
-	printf(",\n%d", (int)header.pixelArrayOffset[2]);
-	printf(",\n%d", (int)header.pixelArrayOffset[3]);
+	printf(",%d", (int)header.pixelArrayOffset[1]);
+	printf(",%d", (int)header.pixelArrayOffset[2]);
+	printf(",%d", (int)header.pixelArrayOffset[3]);
+}
 
+void printDib(struct _dib dib){
+	printf("\n\n***DIB***");
+	printf("\n-Dib size-");
+	printf("\n%d",(int)dib.dibSize[0]);
+	printf(",%d", (int)dib.dibSize[1]);
+	printf(",%d", (int)dib.dibSize[2]);
+	printf(",%d", (int)dib.dibSize[3]);
+	printf("\n-Bitmap width-");
+	printf("\n%d",(int)dib.bitmapWidth[0]);
+	printf(",%d", (int)dib.bitmapWidth[1]);
+	printf(",%d", (int)dib.bitmapWidth[2]);
+	printf(",%d", (int)dib.bitmapWidth[3]);
+	printf("\n-Bitmap height-");
+	printf("\n%d",(int)dib.bitmapHeight[0]);
+	printf(",%d", (int)dib.bitmapHeight[1]);
+	printf(",%d", (int)dib.bitmapHeight[2]);
+	printf(",%d", (int)dib.bitmapHeight[3]);
+	printf("\n-Number colors-");
+	printf("\n%d",(int)dib.numberColors[0]);
+	printf(",%d", (int)dib.numberColors[1]);
+	printf("\n-Number bits x pixel-");
+	printf("\n%d",(int)dib.NumberBitsPerPixel[0]);
+	printf(",%d", (int)dib.NumberBitsPerPixel[1]);
+	printf("\n-BI_RGB-");
+	printf("\n%d",(int)dib.biRGB[0]);
+	printf(",%d", (int)dib.biRGB[1]);
+	printf(",%d", (int)dib.biRGB[2]);
+	printf(",%d", (int)dib.biRGB[3]);
+	printf("\n-Bitmap size-");
+	printf("\n%d",(int)dib.bitmapSize[0]);
+	printf(",%d", (int)dib.bitmapSize[1]);
+	printf(",%d", (int)dib.bitmapSize[2]);
+	printf(",%d", (int)dib.bitmapSize[3]);
+	printf("\n-Print resolution horizontal-");
+	printf("\n%d",(int)dib.printResolutionHorizontal[0]);
+	printf(",%d", (int)dib.printResolutionHorizontal[1]);
+	printf(",%d", (int)dib.printResolutionHorizontal[2]);
+	printf(",%d", (int)dib.printResolutionHorizontal[3]);
+	printf("\n-Print resolution vertical-");
+	printf("\n%d",(int)dib.printResolutionVertical[0]);
+	printf(",%d", (int)dib.printResolutionVertical[1]);
+	printf(",%d", (int)dib.printResolutionVertical[2]);
+	printf(",%d", (int)dib.printResolutionVertical[3]);
+	printf("\n-Number of colors palette-");
+	printf("\n%d",(int)dib.colorsPalette[0]);
+	printf(",%d", (int)dib.colorsPalette[1]);
+	printf(",%d", (int)dib.colorsPalette[2]);
+	printf(",%d", (int)dib.colorsPalette[3]);
+	printf("\n-Importants colors-");
+	printf("\n%d",(int)dib.importantColors[0]);
+	printf(",%d", (int)dib.importantColors[1]);
+	printf(",%d", (int)dib.importantColors[2]);
+	printf(",%d", (int)dib.importantColors[3]);
 }
 
 int getPading(int width){
 	  
 	int rest = width%4;
+	/*
 	if(rest == 0){
 		printf("\nEl ancho de la imagen es multiplo de 4");
 	}else{
 		printf("\nEl resto de la imagen es: %d", rest);
 	}
-	return 0;
+	*/
+	return rest;
 }
 
 
-void printBitmap(struct _bitmap bitmap){
-	printf("\n-Pixel array-\n");
+void printBitmap(Bitmap bitmap){
+	printf("\n\n***PIXEL ARRAY***\n");
 	int i = 0;
-	while(i < bitmap.width*bitmap.height){
+	int offsetIndex = 0;
+	while(i < bitmap->width*bitmap->height){
 		printf("[");
-		int r = (int)(bitmap.pixels + i)->red;
-		int g = (int)(bitmap.pixels + i)->green;
-		int b = (int)(bitmap.pixels + i)->blue;
+		int r = (int)(bitmap->pixels + i)->red;
+		int g = (int)(bitmap->pixels + i)->green;
+		int b = (int)(bitmap->pixels + i)->blue;
 		printf("%d", r);
 		printf(",%d", g);
 		printf(",%d", b);
 		printf("]");
 		i++;
-		if(i % bitmap.width == 0){
+		if(i % bitmap->width == 0){
+			//print offset
+			while(offsetIndex < getPading(bitmap->width)/2){
+				printf("[0,0]");
+				offsetIndex++;
+			}
+			offsetIndex = 0;
 			printf("\n");
 		}
 	}
--- main.c
+++ main.c
@@ -5,22 +5,23 @@
 int main(int argc, char *argv[]) {
 
 	//Create bitmap
-	Bitmap bitmap = buildBitmap(4,2);
+	Bitmap bitmap = buildBitmap(2,2);
 	
 	//Insert pixels
 	/*
 	0,1,0,1
 	1,0,1,1
 	*/
-	color black;
+	//color black;
 	color white;
-	black.red = 0;
-	black.green = 0;
-	black.blue = 0;
+	//black.red = 0;
+	//black.green = 0;
+	//black.blue = 0;
 	white.red = 255;
 	white.green = 255;
 	white.blue = 255;
 
+	/*
 	setPixel(bitmap, 0, 0, black);
 	setPixel(bitmap, 1, 0, white);
 	setPixel(bitmap, 2, 0, black);
@@ -31,7 +32,23 @@ int main(int argc, char *argv[]) {
 	setPixel(bitmap, 2, 1, white);
 	setPixel(bitmap, 3, 1, black);
 
-	
+	*/
+	color blue;
+	color green;
+	color red;
+	blue.red = 0;
+	blue.green = 0;
+	blue.blue = 255;
+	green.red = 0;
+	green.green = 255;
+	green.blue = 0;
+	red.red = 255;
+	red.green = 0;
+	red.blue = 0;
+	setPixel(bitmap, 0, 0, blue);
+	setPixel(bitmap, 1, 0, green);
+	setPixel(bitmap, 0, 1, red);
+	setPixel(bitmap, 1, 1, white);
 
 	//Save bitmap
 	char path[] = "/home/luis/Escritorio/lesson_6/bitmap_project/img_1";
